591ff261ffd0f0eb684d8de994ce15dc1b792ea0
zhutao <zhutao1221@126.com>
2018-04-19 17:02:58 +0800

    lstm

---
+++ src/TensorFlow/src/lstmTest.py
@@ -0,0 +1,75 @@
+# 加载数据分析常用库
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Dropout
+import matplotlib.pyplot as plt
+import glob, os
+import seaborn as sns
+import sys
+from sklearn.preprocessing import MinMaxScaler
+
+#查看其中一个地区的训练数据
+feature=['YEAR','MONTH','DAY','TEMP_HIG','TEMP_COL','AVG_TEMP','AVG_WET','DATA_COL']
+example = pd.read_csv('data/weathor.csv', names=feature)
+# example = pd.read_csv('../input/industry/industry_timeseries/timeseries_train_data/1.csv', 
+#                       names=feature)
+# 将时间序列数据转化为监督问题数据
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+    n_vars = 1 if type(data) is list else data.shape[1]
+    df = pd.DataFrame(data)
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        if i == 0:
+            names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+        else:
+            names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+    # put it all together
+    agg = pd.concat(cols, axis=1)
+    agg.columns = names
+    # drop rows with NaN values
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg
+    
+# 将数据归一化到0-1之间,无量纲化
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(example[['DATA_COL','TEMP_HIG','TEMP_COL','AVG_TEMP','AVG_WET']].values)
+# 将时序数据转换为监督问题数据
+reframed = series_to_supervised(scaled_data, 1, 1)
+#删除无用的label数据
+reframed.drop(reframed.columns[[6,7,8,9]], axis=1, inplace=True)
+# 数据集划分,选取前400天的数据作为训练集,中间150天作为验证集,其余的作为测试集
+train_days = 400
+valid_days = 150
+values = reframed.values
+train = values[:train_days, :]
+valid = values[train_days:train_days+valid_days, :]
+test = values[train_days+valid_days:, :]
+train_X, train_y = train[:, :-1], train[:, -1]
+valid_X, valid_y = valid[:, :-1], valid[:, -1]
+test_X, test_y = test[:, :-1], test[:, -1]
+# 将数据集重构为符合LSTM要求的数据格式,即 [样本，时间步，特征]
+train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+valid_X = valid_X.reshape((valid_X.shape[0], 1, valid_X.shape[1]))
+test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+print(train_X.shape, train_y.shape, valid_X.shape, valid_y.shape, test_X.shape, test_y.shape)
+model = Sequential()
+model.add(LSTM(50, activation='relu',input_shape=(train_X.shape[1], train_X.shape[2])))
+model.add(Dense(1, activation='linear'))
+model.compile(loss='mean_squared_error', optimizer='adam')  #loss='mae'
+model.summary()
+
+# fit network
+history = model.fit(train_X, train_y, epochs=100, batch_size=32, validation_data=(valid_X, valid_y), verbose=2, shuffle=False)
+# plot history
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='valid')
+plt.legend()
+plt.show()
